f4533858e165c5fb4bc3a8ad2838069d68d3dc46
Lenovo T14s <souleymane14131@gmail.com>
2026-07-07 18:05:17 +0000

    chore(customer): edit code

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import express from 'express';
+
+let cachedServer;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-    const config = new DocumentBuilder()
+  const expressApp = express();
+
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://ton-frontend.vercel.app'
+    ],
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
     .setTitle('monBusines')
     .setDescription('mon-business api')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-    app.enableCors({
-    origin: 'http://localhost:4200', // ton frontend Angular
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-  });
-  await app.listen(process.env.PORT ?? 3000);
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.init();
+
+  return expressApp;
 }
-bootstrap();
+
+export default async function handler(req, res) {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+
+  return cachedServer(req, res);
+}
