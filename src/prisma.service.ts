25004d89a93dfe54cd7f0ebf235029c5e7c03ba6
Lenovo T14s <souleymane14131@gmail.com>
2026-07-08 17:11:40 +0000

    chore(prisma): migrate table supabase

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -4,11 +4,16 @@ import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import { url } from 'inspector';
+import { Pool } from 'pg';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    })
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 }
